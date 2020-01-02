2e16a99ccd8052dbefc2be5a0be395e3d5198025
cyiping <yiping.cheng@cyiping.com>
2020-01-02 12:06:52 +0800

    2020/01/02

---
+++ src/course/c06/Question/X.java
@@ -0,0 +1,11 @@
+package course.c06.Question;
+
+public class X {
+
+	public static void main(String[] args) {
+		String s = "Hello Java!";			// 總共11個字元
+		System.out.println(s.charAt(11)); 	// 從0開始算，index只有到10
+
+	}
+
+}
