3e2dcebc2718663d5f4cd98ae1ac0714806155df
cyiping <yiping.cheng@cyiping.com>
2020-01-02 16:04:48 +0800

    2020/01/02

---
+++ src/course/c06/Question/Q9.java
@@ -0,0 +1,16 @@
+package course.c06.Question;
+
+public class Q9 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Drunb is the President!");
+		System.out.println(sb);
+		sb.delete(13, 23);
+		System.out.println(sb);
+		sb.append("Big SB!");
+		System.out.println(sb);
+
+	}
+
+}
