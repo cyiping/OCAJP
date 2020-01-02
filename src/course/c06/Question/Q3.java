d634fd9d56d9df360407a6f97f6367ecb0abffd2
cyiping <yiping.cheng@cyiping.com>
2020-01-02 11:58:17 +0800

    2020/01/02

---
+++ src/course/c06/Question/Q3.java
@@ -0,0 +1,11 @@
+package course.c06.Question;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(" Output: " + 2 + 3 + 4);
+		System.out.print(" Output: " + 2 + 3 * 4);
+	}
+
+}
